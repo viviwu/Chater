3cf5f51ef7731b9379e54b029bd3c210f037bffd
viviwu <vivi705@qq.com>
2017-07-14 17:03:07 +0800

    Chater is an IM app with XWVoIPKit
    Chater is an IM app with XWVoIPKit designed for iPad by vivi.

---
+++ Chater/APL/APLPrivacyDetailViewController.h
@@ -0,0 +1,17 @@
+/*
+ Copyright (C) 2017 Apple Inc. All Rights Reserved.
+ See LICENSE.txt for this sample’s licensing information
+ 
+ Abstract:
+ View controller that handles checking and requesting access to the users private data classes.
+ */
+
+@import UIKit;
+
+typedef void (^CheckAccessBlock)();
+typedef void (^RequestAccessBlock)();
+
+@interface APLPrivacyDetailViewController : UITableViewController
+@property (nonatomic, copy) CheckAccessBlock checkBlock;
+@property (nonatomic, copy) RequestAccessBlock requestBlock;
+@end
---
+++ Chater/XWTapViewController.h
@@ -0,0 +1,13 @@
+//
+//  XWTapViewController.h
+//  iPadUIdesign
+//
+//  Created by Qway on 2017/7/13.
+//  Copyright © 2017年 viviwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XWTapViewController : UIViewController
+
+@end
